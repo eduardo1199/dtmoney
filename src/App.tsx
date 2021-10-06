ae671c871feab1888b8b047a4e86d601dcf95bd2
eduardo1199 <eduardo.soares.116@ufrn.edu.br>
2021-10-05 22:01:53 -0300

    Criação de conexão com mirage e modal do material

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { createServer } from 'miragejs';
 import { GlobalStyle } from './styles/global';
 
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
+
+import { transactionsData } from './data/transactionsData';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+    this.get('/transactions', () => transactionsData);
+  },
+});
 
 export function App() {
   return (
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,32 @@
+import { useState } from 'react';
+import { Modal } from '@material-ui/core';
 import logo from '../../assets/logo.svg';
 
 import { HeaderContainer, Content } from './styles';
 
 export function Header() {
+  const [isNewTransactionModaOpen, SetIsNewTransactionModaOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    SetIsNewTransactionModaOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    SetIsNewTransactionModaOpen(false);
+  }
+
   return (
     <HeaderContainer>
       <Content>
         <img src={logo} alt="dt money" />
-        <button type="button">Nova transação</button>
+        <button type="button" onClick={handleOpenNewTransactionModal}>Nova transação</button>
       </Content>
+      <Modal
+        open={isNewTransactionModaOpen}
+        onClose={() => handleCloseNewTransactionModal()}
+      >
+        <h2>Cadastrar Transação</h2>
+      </Modal>
     </HeaderContainer>
   );
 }
